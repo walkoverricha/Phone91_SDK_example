b6395a285a24e8a39b2c6745ec7fed6b9fd75d55
richa <richa@walkover.in>
2020-07-04 18:44:08 +0530

    First Commit

--- app/src/main/java/com/phone91/sample/MainActivity.kt
+++ app/src/main/java/com/phone91/sample/MainActivity.kt
@@ -4,16 +4,34 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.phone91.sdk.mvvm.dashboard.DashboardActivity
+
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private var widgetToken= "Pass your widget token here"
+
+//    Set below values, if you do not ask user for these detail
+    /*private var name= "write name"
+    private var email= "write email"
+    private var phone= "write pnone number"*/
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         btnTest.setOnClickListener{
             var intent= Intent(this,DashboardActivity::class.java)
-            intent.putExtra(DashboardActivity.WIDGETTOKEN,"28b43")
+            intent.putExtra(DashboardActivity.WIDGETTOKEN,widgetToken)
+
+    //Uncomment below code, if you want to pass login detail of user
+
+//            intent.putExtra(DashboardActivity.NAME,name)
+//            intent.putExtra(DashboardActivity.EMAIL,email)
+//           intent.putExtra(DashboardActivity.PHONE,phone)
+
+
             startActivity(intent)
         }
     }
